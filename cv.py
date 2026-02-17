f9b0d92ed144038b0e10d34ad8df0cfa2b6b7acc
mameawadiallo03-code <mameawadiallo03@gmail.com>
2026-02-18 00:24:33 +0100

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,43 @@
+import streamlit as st
+with st.sidebar:
+    st.write("Prénom et Nom: Mame Awa Diallo")
+    st.write("Géomaticienne")
+    st.write("Téléphone:781260437")
+    st.write("Email:mameawadiallo03@gmail.com")
+    
+    
+st.markdown("---")
+
+
+st.header("À propos de moi")
+
+st.write("Technicienne Supérieure en Géomatique, je suis passionnée par  l'analyse spatial. j'ai des base en python, en la collecte de données au terrain et en l'utilisation d'outils comme QGIS.je désir  renforcé mes compétences à travers des stage et projets pratique.")
+st.markdown("---")
+
+st.header("FORMATIONS")
+st.write("Brevet de Technicienne Supérieure en Géomatique")
+st.write("Baccalauréat scientifique")
+st.write("Mention Assez bien")
+
+st.markdown("---")
+
+st.header("COMPETENCES")
+
+
+st.write("Numérisation")
+st.write("suite bureaurique")
+st.write("cartographie")
+st.write("Base en Python")
+st.markdown("---")
+
+st.header("PROJETS ACADEMIQUES")
+
+
+st.write("Levées Topographiques")
+st.write("Collecte de données au CEDT,à Massalikul Jinaan et dans quelques communes de la region.")
+st.write("Réalisation de carte thématique, les différents types de sols de la région de Kolda.")
+st.write("Conception d'un plan architechtural:RDC,Terasse,Masse,Facade,Coupe")
+st.write("Réalisation d'applications de calculs avec Streamlit et Idle")
+st.markdown("---")
+
+
